b2397f8f5b1ff3fb64e1f1a8cd10633153b74782
Samuel Russek <samuel_bstone_13@hotmail.com>
2023-05-22 18:56:13 -0600

    progress on events models

--- models/events.js
+++ models/events.js
@@ -1,5 +1,87 @@
-const { Schema,} = require('mongoose');
+const { Schema, model } = require('mongoose');
 
-const reactionSchema = new Schema 
 
-    module.exports = reactionSchema
+const commentSchema = new Schema (
+    {
+    eventName: {
+        type: String,
+        required: true,
+        maxLength: 50,
+        minLength: 1
+
+    },
+    eventDescription: {
+        type: String,
+        required: true,
+        maxlength: 280,
+        minlength: 1
+    },
+    mainImg: {
+        type: String,
+
+
+    },
+    portraitImg: {
+        type: String,
+
+
+    },
+    tags: {
+
+
+    },
+    usersAssisting: {
+        type: String,
+
+    },
+    
+    eventStartDate: {
+        type: Date,
+        required: true,
+    },
+    eventLocation: {
+        type: String,
+        required: true,
+        maxlength: 280,
+        minlength: 1
+        
+    },
+    eventType: {
+        type: String,
+        type: String,
+        required: true,
+        maxlength: 280,
+        minlength: 1
+    },
+    
+    
+    eventCapacity: {
+        type: String,
+        type: String,
+        required: true,
+        maxlength: 280,
+        minlength: 1
+    },
+    eventInvitation: {
+
+
+    }
+
+},
+createdAt: {
+    type: Date, 
+    default: Date.now,
+},
+{
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  },
+
+)
+
+
+
+const Events = model('Events', commentSchema);
+module.exports = Events;

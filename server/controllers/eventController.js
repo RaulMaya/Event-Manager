c2cd4c58434354f6baf787fb36bb6feaaf4a04dc
Drums180 <daviddomor180@hotmail.com>
2023-05-24 18:37:30 -0600

    Controllers updated, CRUD for all 3 models

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -1,66 +1,79 @@
-const { Comment, User, Events } = require('../models');
-//we will probably add stuff with user and events later.
+const { Events } = require("../models");
 
 const eventController = {
-  
+  // GET all events
   async getEvents(req, res) {
     try {
       const events = await Events.find();
-      res.json(users);
+      res.json(events);
     } catch (err) {
       res.status(500).json(err);
     }
   },
+
+  // GET a single event by id
   async getSingleEvent(req, res) {
     try {
-      const events = await Events.findOne({ _id: req.params.userId })
-        .select('-__v')
+      const event = await Events.findOne({ _id: req.params.eventId }).select(
+        "-__v"
+      );
 
-
-      if (!events) {
-        return res.status(404).json({ message: 'No event with that ID' });
+      if (!event) {
+        return res.status(404).json({ message: "No event with that ID" });
       }
 
-      res.json(Events);
+      res.json(event);
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
     }
-
   },
-  // create a new user
+
+  // CREATE a new event
   async createEvent(req, res) {
     try {
       console.log(req.body);
-      const dbUserData = await Event.create(req.body);
-      res.json(dbUserData);
+      const dbEventData = await Events.create(req.body);
+      res.json(dbEventData);
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
     }
   },
+
+  // UPDATE an existing event by id
   async updateEvent(req, res) {
-    const dbUserData = await Event.findOneAndUpdate(
-      { _id: req.params.userId },
-      { $set: req.body },
-      { new: true }
-    );
-    res.json (dbUserData);
-
+    try {
+      const dbEventData = await Events.findOneAndUpdate(
+        { _id: req.params.eventId },
+        { $set: req.body },
+        { new: true }
+      );
+      if (!dbEventData) {
+        return res.status(404).json({ message: "No event with that ID" });
+      }
+      res.json(dbEventData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
   },
-  async deleteEvent(req, res) {
-    const dbUserData = await Event.findOneAndDelete(
-      { _id: req.params.userId },
-      { $set: req.body },
-      { new: true }
-  );
-  if (!dbUserData) {
-    return res.status(404).json({ message: 'No event with that ID' });
-  }
-  res.json (dbUserData);
-  
-},
 
+  // DELETE an event by id
+  async deleteEvent(req, res) {
+    try {
+      const dbEventData = await Events.findOneAndDelete({
+        _id: req.params.eventId,
+      });
+      if (!dbEventData) {
+        return res.status(404).json({ message: "No event with that ID" });
+      }
+      res.json(dbEventData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
 };
 
 module.exports = eventController;

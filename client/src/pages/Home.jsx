8882475e6d96d4b43bd7b0e3e5a58af62e9a6c0f
Raul <raulmayas20@gmail.com>
2023-05-29 10:51:53 -0600

    Query events on react front-end page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     const { loading, error, data } = useQuery(QUERY_ALL_EVENTS);
 
     console.log('Event Data:', data); // Display event data in the console
-
+    data.events.map((event) => console.log(event.location))
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error :(</p>;
 
@@ -15,6 +15,7 @@ const Home = () => {
             <h1 className="my-4">Events</h1>
             <div className="row">
                 {data.events.map((event) => (
+
                     <div key={event._id} className="col-md-4 mb-4">
                         <div className="card">
                             <img src={event.mainImg} className="card-img-top" alt={event.eventName} />
@@ -24,9 +25,9 @@ const Home = () => {
                                 <p className="card-text">
                                     <strong>Date:</strong> {event.eventStartDate}
                                 </p>
-                                <p className="card-text">
+                                {/* <p className="card-text">
                                     <strong>Location:</strong> {event.eventLocation.city}, {event.eventLocation.country}
-                                </p>
+                                </p> */}
                                 <p className="card-text">
                                     <strong>Type:</strong> {event.eventType}
                                 </p>

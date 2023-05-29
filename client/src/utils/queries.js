b2554167e2e67d8949eb58113d10d2a0120d3738
Raul <raulmayas20@gmail.com>
2023-05-28 22:46:42 -0600

    Including Queries (Front-End) and implementing Auth (Initialization)

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,11 +1,142 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
-export const QUERY_PROFILES = gql`
-  query allProfiles {
-    profiles {
+export const QUERY_ALL_USERS = gql`
+  query allUsers {
+    users {
       _id
-      name
-      skills
+      username
+      email
+      dateOfBirth
+      profilePic
+    }
+  }
+`;
+
+export const QUERY_SINGLE_USER = gql`
+  query singleUser($userId: ID!) {
+    user(id: $userId) {
+      _id
+      username
+      email
+      password
+      dateOfBirth
+      profilePic
+      comments {
+        commentText
+        event {
+          _id
+          eventName
+        }
+      }
+      friends {
+        username
+        _id
+      }
+      assistingEvents {
+        _id
+        eventName
+      }
+      createdEvents {
+        _id
+        eventName
+      }
+    }
+  }
+`;
+
+export const QUERY_ALL_EVENTS = gql`
+  query allEvents {
+    events {
+      _id
+      eventName
+      mainImg
+      createdBy {
+        _id
+        username
+      }
+      eventStartDate
+      eventInvitation
+      minAge
+    }
+  }
+`;
+
+export const QUERY_SINGLE_EVENT = gql`
+  query singleEvent($eventId: ID!) {
+    event(id: $eventId) {
+      _id
+      eventName
+      eventDescription
+      eventCapacity
+      mainImg
+      portraitImg
+      eventLocation {
+        address
+        city
+        country
+        state
+        lat
+        lon
+      }
+      eventStartDate
+      eventType
+      eventInvitation
+      minAge
+      tags
+      createdBy {
+        _id
+        username
+      }
+      createdAt
+      usersAssisting {
+        _id
+        username
+      }
+      comments {
+        _id
+        user {
+          _id
+          username
+        }
+        commentText
+        createdAt
+      }
+    }
+  }
+`;
+
+export const QUERY_ALL_COMMENTS = gql`
+  query allComments{
+    comments {
+      _id
+      commentText
+      createdAt
+      user {
+        _id
+        username
+      }
+      event {
+        _id
+        eventName
+      }
+    }
+  }f
+`;
+
+export const QUERY_SINGLE_COMMENT = gql`
+  query singleComment($commentId: ID!) {
+    comment(id: $commentId) {
+      _id
+      commentText
+      createdAt
+      user {
+        _id
+        username
+      }
+      event {
+        _id
+        eventName
+      }
     }
   }
 `;

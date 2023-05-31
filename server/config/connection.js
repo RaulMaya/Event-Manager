75514ad77b43df1e7e6062bbd9290481fca76724
Drums180 <daviddomor180@hotmail.com>
2023-05-31 09:13:22 -0600

    Prepare for Heroku deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
-const mongoose = require('mongoose');
+require("dotenv").config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/partymaster');
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/partymaster");
 
 module.exports = mongoose.connection;

44cb7288207f2aec1dc904d7455551ac1a5a30cb
Drums180 <daviddomor180@hotmail.com>
2023-05-31 09:39:24 -0600

    Prepare for Heroku deployment

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,5 @@
 const Comment = require("./Comment");
 const Event = require("./Event");
-const User = require("./User");
+const { User } = require("./User"); // Desestructura el objeto y obtén el modelo User
 
 module.exports = { Comment, Event, User };

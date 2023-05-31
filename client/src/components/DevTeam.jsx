1131ddf0c0f1c15da18b97dd229678277979f27e
Raul <raulmayas20@gmail.com>
2023-05-31 17:19:50 -0600

    Rendering developer

---
+++ client/src/components/DevTeam.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { VStack, Flex, Image, Text, Link, Box } from "@chakra-ui/react";
+import { FaGithub, FaWhatsapp, FaEnvelope } from 'react-icons/fa';
+
+const DeveloperCard = ({ developer }) => {
+    const boxBg = 'white'; // Set the background color of the card
+    const mainText = 'black'; // Set the color of the main text
+    const secondaryText = 'gray'; // Set the color of the secondary text
+
+    return (
+        <Box
+            borderRadius='20px'
+            bg={boxBg}
+            p='20px'
+            h='345px'
+            w={{ base: "315px", md: "345px" }}
+            alignItems='center'
+            direction='column'
+            style={{
+                boxShadow: '0px 8px 16px rgba(0, 0, 0, 0.1)', // Add aesthetic shadow
+            }}
+            m="20px" // Add separation between cards
+        >
+            <Image
+                src='https://i.ibb.co/xmP2pS6/Profile.png'
+                maxW='100%'
+                borderRadius='20px'
+            />
+            <Flex flexDirection='column' mb='30px'>
+                <Image
+                    src={developer.image}
+                    border='5px solid red'
+                    mx='auto'
+                    borderColor={boxBg}
+                    width='68px'
+                    height='68px'
+                    mt='-38px'
+                    borderRadius='50%'
+                />
+                <Text
+                    fontWeight='600'
+                    color={mainText}
+                    textAlign='center'
+                    fontSize='xl'
+                >
+                    {developer.name}
+                </Text>
+                <Text
+                    color={secondaryText}
+                    textAlign='center'
+                    fontSize='lg'
+                    fontWeight='500'
+                >
+                    {developer.position}
+                </Text>
+            </Flex>
+            <Flex justify='space-between' w='100%' px='36px'>
+                <Flex flexDirection='column' alignItems='center'>
+                    <Link
+                        href={developer.github}
+                        isExternal
+                        _hover={{ color: "red.500", textDecoration: "none" }}
+                    >
+                        <FaGithub size={30} color="purple" style={{ transition: '0.3s' }} />
+                    </Link>
+                    <Text color={secondaryText} fontWeight='500'>
+                        GitHub
+                    </Text>
+                </Flex>
+                <Flex flexDirection='column' alignItems='center'>
+                    <Link
+                        href={`https://wa.me/${developer.whatsapp}`}
+                        isExternal
+                        _hover={{ color: "red.500", textDecoration: "none" }}
+                    >
+                        <FaWhatsapp size={30} color={"green"} style={{ transition: '0.3s' }} />
+                    </Link>
+                    <Text color={secondaryText} fontWeight='500'>
+                        WhatsApp
+                    </Text>
+                </Flex>
+                <Flex flexDirection='column' alignItems='center'>
+                    <Link
+                        href={`mailto:${developer.email}`}
+                        isExternal
+                        _hover={{ color: "red.500", textDecoration: "none" }}
+                    >
+                        <FaEnvelope size={30} color={"gold"} style={{ transition: '0.3s' }} />
+                    </Link>
+                    <Text color={secondaryText} fontWeight='500'>
+                        Email
+                    </Text>
+                </Flex>
+            </Flex>
+
+        </Box>
+    );
+}
+
+export default DeveloperCard
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { FiTrendingUp } from 'react-icons/fi';
+
 import { useQuery } from '@apollo/client';
+import { Box, Heading, Spinner, Alert, Flex } from '@chakra-ui/react';
+
 import { QUERY_ALL_EVENTS } from '../utils/queries';
+import AuthService from '../utils/auth';
+
 import EventList from '../components/EventList';
 import HeroSection from '../components/Hero';
-import { Box, Heading, Spinner, Alert } from '@chakra-ui/react';
-import { FiTrendingUp } from 'react-icons/fi';
-import AuthService from '../utils/auth';
+import DeveloperCard from '../components/DevTeam';
 
 const Home = () => {
     const { loading, error, data } = useQuery(QUERY_ALL_EVENTS);
@@ -21,6 +25,49 @@ const Home = () => {
         return <Alert status="error">{error.message} :(</Alert>;
     }
 
+    const developers = [
+        {
+            name: "Raul Maya",
+            image: "team/raulmaya.jpeg",
+            position: "Web Developer",
+            github: "https://github.com/RaulMaya",
+            whatsapp: "+528331597006",
+            email: "raulmayas20@gmail.com",
+        },
+        {
+            name: "David Dominguez",
+            image: "team/daviddominguez.jpg",
+            position: "Web Developer",
+            github: "https://github.com/Drums180",
+            whatsapp: "+528120384836",
+            email: "./Projects/MarsScraping.png",
+        },
+        {
+            name: "Samuel Russek",
+            image: "team/samuelrussek.jpg",
+            position: "Web Developer",
+            github: "https://github.com/SamRF13",
+            whatsapp: "+525535211344",
+            email: "./Projects/NotesApp.png",
+        },
+        {
+            name: "Freddy Corona",
+            image: "team/freddycorona.jpg",
+            position: "Web Developer",
+            github: "https://github.com/cryptovoyager",
+            whatsapp: "+525584834472",
+            email: "./Projects/NuevoLeonElections.png",
+        },
+        {
+            name: "Saul Wade",
+            image: "team/saulwade.jpg",
+            position: "Web Developer",
+            github: "https://github.com/saulwade",
+            whatsapp: "+529221574450",
+            email: "./Projects/SoccerQuiz.png",
+        }
+    ];
+
     return (
         <Box maxW="container-fluid" mx="auto" p={4}>
             <HeroSection />
@@ -33,6 +80,11 @@ const Home = () => {
             <Box mb={4}>
                 <EventList events={events} title="All our events..." isAuthenticated={isAuthenticated} showAllEvents={false} />
             </Box>
+            <Flex bg="purple.500"  justify='center' align='center' direction='row' wrap='wrap'>
+                {developers.map((developer, index) => (
+                    <DeveloperCard key={index} developer={developer} />
+                ))}
+            </Flex>
         </Box>
     );
 };

2165f30c2dc5354e79847f0b5bd1340ae73e23c6
Raul <raulmayas20@gmail.com>
2023-07-10 11:53:02 -0600

    Including CORS Library

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
+const cors = require('cors'); // Import cors
 const { authMiddleware } = require("./utils/auth");
 
 const { typeDefs, resolvers } = require("./schemas");
@@ -8,6 +9,7 @@ const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(cors()); // Use cors middleware
 const server = new ApolloServer({
   typeDefs,
   resolvers,
